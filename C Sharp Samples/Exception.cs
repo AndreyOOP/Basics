9de207419a92c2e416f28dce1b504f1cecb1cb40
Andrii Yudaichev <yudaichev.andrii@pdffiller.team>
2022-12-16 02:07:36 +0200

    Add exception, property switch samples

---
+++ C Sharp Samples/Exception.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Samples
+{
+    [TestClass]
+    public class ExceptionSample
+    {
+        [TestMethod]
+        public void CatchWhen()
+        {
+            try
+            {
+                //throw new ArgumentException() { Source = "test" };
+                throw new ArgumentException("msg");
+                //throw new Exception();
+            }
+            catch (ArgumentException ex) when (ex.Source == "test")
+            {
+            }
+            catch (ArgumentException ex) when (ex.Message == "msg")
+            {
+            }
+            catch (ArgumentException ex) 
+            {
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                // The only things that can defeat a finally block are an infinite loop or the process
+                // ending abruptly
+            }
+        }
+
+        [TestMethod]
+        public void ThrowVsThrowEx()
+        {
+            try
+            {
+                Throw();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            try
+            {
+                ThrowEx();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        private void Throw()
+        {
+            try
+            {
+                var x = 0;
+                Console.WriteLine(1 / x);
+            }
+            catch (Exception ex)
+            {
+                // 1. Define more specific exception
+                // 2. Do some action (e.g logging) before rethrowing
+                throw; // correctly point stacktrace to (1/x line)
+            }
+        }
+
+        private void ThrowEx()
+        {
+            try
+            {
+                var x = 0;
+                Console.WriteLine(1 / x);
+            }
+            catch (Exception ex)
+            {
+                throw ex; // stacktrace points that error appears here (instead of 1/x line)
+            }
+        }
+    }
+}
---
+++ C Sharp Samples/PropertySamples.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace C_Sharp_Samples
+{
+    [TestClass]
+    public class PropertySamples
+    {
+        [TestMethod]
+        public void InitProperty()
+        {
+            var a = new Properties { A = 10 };
+            var b = new Properties(1, 2);
+        }
+    }
+
+    public class Properties
+    {
+        public int A { get; init; }
+        public int B { get; private set; }
+
+        public Properties() {}
+
+        public Properties(int a, int b)
+        {
+            A = a;
+            B = b;
+        }
+    }
+}
